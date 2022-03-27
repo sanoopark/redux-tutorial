9cc775ec33f54dd5e9d32230543779d588e7c669
sanoopark <sanoopark@gmail.com>
2022-03-27 20:49:19 +0900

    Refactor: Redux 적용 후

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// reducer : 모든 데이터 변경에 대한 책임 -> 새로운 상태를 반환
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") return count + 1;
+  if (action.type === "MINUS") return count - 1;
+  return count;
 };
 
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+// subscribe : 상태 변경 후 실행
+countStore.subscribe(onChange);
+
 const handleAdd = () => {
-  count++;
-  updateText();
+  // dispatch : reducer에 action 전송
+  countStore.dispatch({ type: "ADD" });
 };
 
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 
 add.addEventListener("click", handleAdd);
